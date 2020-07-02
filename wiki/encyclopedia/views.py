a4138bc541d740db4db5dc31291f361cbf211a7d
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-07-02 12:38:19 -0500

    Comments added

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -6,12 +6,13 @@ import random
 
 
 def index(request):
+    #Send a list of all entries and a random entry.
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
         "random": random.choice(util.list_entries())
     })
 def entry(request,selection):
-
+    #Send data about the selected entry in html format.
     if util.get_entry(selection) != None:
         context = {
             "selection":selection,
@@ -25,6 +26,7 @@ def entry(request,selection):
 def search(request):
     query = request.POST.get("q")
     print(query)
+    #If the entry exists send person to the entry, else show them possible options
     if util.get_entry(query) != None:
         return redirect('entry',selection=query)
     else:
@@ -34,6 +36,7 @@ def search(request):
         }
         return render(request,"encyclopedia/search.html",context)
 def edit(request,selection):
+    #If the user has submitted a change, update it, else display the current page in markdown format.
     if request.method == "POST":
         util.save_entry(selection,request.POST.get("data"))
         return redirect('entry',selection=selection)
@@ -48,6 +51,7 @@ def edit(request,selection):
         else:
             raise Http404(request,f"a page for {selection} does not exist.")
 def new(request):
+    #If the user submits a new entry, check if the entry exists, else create it. If the user gets the page, submit a creation form.
     if request.method == "POST":
         Name = request.POST.get("name")
         Data = request.POST.get("data")
