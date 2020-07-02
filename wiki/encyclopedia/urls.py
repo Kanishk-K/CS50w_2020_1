f7718482c00c24a94e1095df7ba01bedefe85af6
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-07-02 12:02:19 -0500

    Finished project

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<selection>/",views.entry,name="entry"),
+    path("wiki/<selection>/",views.entry,name="entry"),
+    path("edit/<selection>/",views.edit,name="edit"),
+    path("new",views.new,name="new"),
+    path("search",views.search,name="search")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404,redirect
 
 from . import util
+import markdown2
+import random
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "random": random.choice(util.list_entries())
     })
+def entry(request,selection):
 
+    if util.get_entry(selection) != None:
+        context = {
+            "selection":selection,
+            "data":markdown2.markdown(util.get_entry(selection)),
+            "random": random.choice(util.list_entries())
+        }
+        return render(request,"encyclopedia/entry.html",context)
+    else:
+        raise Http404(request,f"a page for {selection} does not exist.")
+
+def search(request):
+    query = request.POST.get("q")
+    print(query)
+    if util.get_entry(query) != None:
+        return redirect('entry',selection=query)
+    else:
+        context = {
+            "similar":[entry for entry in util.list_entries() if query.lower() in entry.lower()],
+            "random": random.choice(util.list_entries())
+        }
+        return render(request,"encyclopedia/search.html",context)
+def edit(request,selection):
+    if request.method == "POST":
+        util.save_entry(selection,request.POST.get("data"))
+        return redirect('entry',selection=selection)
+    else:
+        if util.get_entry(selection) != None:
+            context = {
+                "selection":selection,
+                "data":util.get_entry(selection),
+                "random": random.choice(util.list_entries())
+            }
+            return render(request,"encyclopedia/edit.html",context)
+        else:
+            raise Http404(request,f"a page for {selection} does not exist.")
+def new(request):
+    if request.method == "POST":
+        Name = request.POST.get("name")
+        Data = request.POST.get("data")
+        if util.get_entry(Name) != None:
+            raise Http404(request,f"a page for {Name} already exists")
+        else:
+            util.save_entry(Name,Data)
+            return redirect('entry',selection=Name)
+    else:
+        return render(request,"encyclopedia/new.html")
